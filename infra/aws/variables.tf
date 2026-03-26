cdd6daa4a06c830b82ac021ffc918958611db612
kienn6034 <kevindev6034@gmail.com>
2026-03-26 07:31:23 +0000

    fix: add backend_image and frontend_image variable declarations

--- infra/aws/variables.tf
+++ infra/aws/variables.tf
@@ -64,3 +64,13 @@ variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
 }
+
+variable "backend_image" {
+  description = "Docker image URI for the backend container"
+  type        = string
+}
+
+variable "frontend_image" {
+  description = "Docker image URI for the frontend container"
+  type        = string
+}
